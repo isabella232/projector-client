2a90f3384e52cec4dd0c855095c402e6ed9823c6
Art Pinch <leonardo906@mail.ru>
2022-02-09 17:51:57 +0300

    Move build version testing function from server to client repo

--- projector-util-loading/build.gradle.kts
+++ projector-util-loading/build.gradle.kts
@@ -39,7 +39,6 @@ val coroutinesVersion: String by project
 val intellijPlatformVersion: String by project
 
 dependencies {
-  compileOnly(project(":projector-common"))
   api(project(":projector-util-logging"))
 
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
